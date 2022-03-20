939b226e1f0347fd497e036257f3c748655e4178
Saud Ahmed Abbasi (DeijiOkzen) <75293238+deijiokzen@users.noreply.github.com>
2022-03-20 15:49:41 +0500

    Add files via upload

---
+++ HuTaoRestaurant/admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">Developed By- <a href="#"> Arun and Saud</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
